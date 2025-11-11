d341586718e302fdd8b5a301675279d0580595e6
winterwind999 <realjordanfaciol@gmail.com>
2025-11-11 09:36:29 +0800

    added prewarm to backend 3

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  webpack(config, { isServer }) {
-    if (isServer) {
-      // When the Next.js server is starting (cold start), ping backend
-      fetch(BACKEND_URL)
-        .then(() => console.log("Pre-warmed backend"))
-        .catch(() => console.log("Failed to pre-warm backend"));
-    }
-    return config;
-  },
+  turbopack: {},
 };
+
+if (typeof fetch !== "undefined") {
+  fetch(BACKEND_URL)
+    .then(() => console.log("Pre-warmed backend"))
+    .catch(() => console.log("Failed to pre-warm backend"));
+}
 
 export default nextConfig;
