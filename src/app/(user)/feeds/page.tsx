c4e8b1617d9c6c45411ecb8dcbd9d47605ddce29
winterwind999 <realjordanfaciol@gmail.com>
2025-11-15 13:53:40 +0800

    wtf 2

--- src/app/(user)/feeds/page.tsx
+++ src/app/(user)/feeds/page.tsx
@@ -1,24 +1,6 @@
-// import { getFeedsAction } from "@/actions/feeds-action";
-// import { type User } from "@/utils/constants";
-// import Feeds from "./feeds";
-
-// export default async function FeedsPage() {
-//   const data = await getFeedsAction();
-
-//   const {
-//     feeds,
-//     message,
-//     total,
-//   }: { feeds: User[]; message: string; total: number } = data;
-
-//   return (
-//     <Feeds initialFeeds={feeds} initialMessage={message} initialTotal={total} />
-//   );
-// }
-
 "use client";
 
-import { BACKEND_URL, User } from "@/utils/constants";
+import { BACKEND_URL, type User } from "@/utils/constants";
 import { useEffect, useState } from "react";
 import Feeds from "./feeds";
 
--- src/utils/getCurrentUserId.ts
+++ src/utils/getCurrentUserId.ts
@@ -1,5 +1,3 @@
-"use server";
-
 import { jwtDecode } from "jwt-decode";
 import { cookies } from "next/headers";
 
